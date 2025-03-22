552593c985259eec1660e5ceef886838bb94e340
AidanZealley <aidan@zealley.com>
2025-03-22 21:56:20 +0000

    Fix auto zooming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
       className={`${GeistSans.variable}`}
       suppressHydrationWarning
     >
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </head>
       <body className="relative">
         <ThemeProvider
           attribute="class"
