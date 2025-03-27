0466feb7717fffdc09653e0ca311268385c5e4cb
AidanZealley <aidan@zealley.com>
2025-03-27 20:41:14 +0000

    Remove old item menu

--- src/app/(app)/items/_components/ItemsListItemMenu/ItemsListItemMenu.types.ts
+++
@@ -1,6 +0,0 @@
-import { ArrayElement } from "@/lib/types";
-import { RouterOutputs } from "@/trpc/react";
-
-export type ItemsListItemMenuProps = {
-  item: ArrayElement<RouterOutputs["items"]["getAll"]>;
-};
