b5eb59b7165c0336342e098cb8eeccb310bd23f4
AidanZealley <aidan@zealley.com>
2025-03-29 14:17:51 +0000

    Fix build errors in main (#16)

--- src/app/(app)/items/_components/ItemAmountAvailable/ItemAmountAvailable.types.ts
+++ src/app/(app)/items/_components/ItemAmountAvailable/ItemAmountAvailable.types.ts
@@ -1,5 +1,5 @@
-import { ArrayElement } from "@/lib/types";
-import { RouterOutputs } from "@/trpc/react";
+import type { ArrayElement } from "@/lib/types";
+import type { RouterOutputs } from "@/trpc/react";
 
 export type ItemAmountAvailableProps = {
   item: ArrayElement<RouterOutputs["items"]["getAll"]>;
