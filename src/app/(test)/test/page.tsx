dafffc58d7b9fb08123246ce6d3cf4094d1117e3
AidanZealley <aidan@zealley.com>
2025-04-18 16:07:29 +0100

    Make test page similar to real one

--- src/app/(test)/test/page.tsx
+++ src/app/(test)/test/page.tsx
@@ -2,11 +2,13 @@ import { NewItemButton } from "@/app/(app)/items/_components/NewItemButton";
 
 export default async function TestPage() {
   return (
-    <main className="grid gap-6 py-6">
-      <div className="relative grid h-[9999px] gap-6 px-6 py-2">
-        <h1 className="text-3xl font-bold">Items</h1>
-        <NewItemButton />
-      </div>
-    </main>
+    <>
+      <main className="grid gap-6 py-6">
+        <div className="relative grid h-[9999px] gap-6 px-6 py-2">
+          <h1 className="text-3xl font-bold">Items</h1>
+        </div>
+      </main>
+      <NewItemButton />
+    </>
   );
 }
