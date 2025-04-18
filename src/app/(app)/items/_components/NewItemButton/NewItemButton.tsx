01e283868e6acbb96fc9dbdafda47c0363a023a9
AidanZealley <aidan@zealley.com>
2025-04-18 15:37:57 +0100

    Make test page similar to real one

--- src/app/(app)/items/_components/NewItemButton/NewItemButton.tsx
+++ src/app/(app)/items/_components/NewItemButton/NewItemButton.tsx
@@ -13,7 +13,7 @@ export const NewItemButton = () => {
   };
 
   return (
-    <div className="sticky right-6 bottom-6 justify-self-end">
+    <div className="fixed right-6 bottom-6 justify-self-end">
       <NewItemDrawer
         open={open}
         onOpenChange={setOpen}
