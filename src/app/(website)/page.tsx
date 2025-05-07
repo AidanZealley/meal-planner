359533eff61f84746192033cf7be50a56050f844
AidanZealley <aidan@zealley.com>
2025-05-07 07:23:08 +0100

    Make home page scrollable

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -2,9 +2,13 @@ import { NewItemButton } from "../(app)/items/_components/NewItemButton";
 
 export default async function DashboardPage() {
   return (
-    <>
-      <span>Home</span>
-      <NewItemButton />
-    </>
+    <div className="relative grid gap-6">
+      <div className="h-[9999px]">
+        <span>Home</span>
+      </div>
+      <div className="sticky right-6 bottom-6 justify-self-end">
+        <NewItemButton />
+      </div>
+    </div>
   );
 }
