1da508dda9788f0d501ac909aa35b1b3d2bdcbea
AidanZealley <aidan@zealley.com>
2025-05-09 07:24:56 +0100

    Use suspense

--- src/app/(app)/planner/cooked/page.tsx
+++ src/app/(app)/planner/cooked/page.tsx
@@ -1,5 +1,6 @@
 import { api, HydrateClient } from "@/trpc/server";
 import { PlannedMealsList } from "../_components/PlannedMealsList";
+import { Suspense } from "react";
 
 export default async function PlannerPage() {
   void api.plannedMeals.getAllByStatus.prefetch({
@@ -10,7 +11,9 @@ export default async function PlannerPage() {
     <HydrateClient>
       <main className="grid gap-6 p-6">
         <h1 className="text-3xl font-bold">Meal Planner</h1>
-        <PlannedMealsList status="cooked" />
+        <Suspense>
+          <PlannedMealsList status="cooked" />
+        </Suspense>
       </main>
     </HydrateClient>
   );
