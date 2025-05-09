96312114b395abe13f189ac6faaf16d1a9a40fc9
AidanZealley <aidan@zealley.com>
2025-05-09 07:19:44 +0100

    Name loading pages

--- src/app/(app)/items/loading.tsx
+++ src/app/(app)/items/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@/components/Spinner";
 
 export default function Loading() {
   return (
-    <div className="grid min-h-0 place-items-center p-6">
+    <div className="grid place-items-center p-6">
       <div className="grid h-full min-h-0 place-items-center p-6">
         <div className="grid justify-items-center gap-3">
           <Spinner />
--- src/app/(app)/profile/loading.tsx
+++ src/app/(app)/profile/loading.tsx
@@ -1,8 +1,8 @@
 import { Spinner } from "@/components/Spinner";
 
-export default function Loading() {
+export default function LoadingProfile() {
   return (
-    <div className="grid min-h-0 place-items-center p-6">
+    <div className="grid place-items-center p-6">
       <div className="grid h-full min-h-0 place-items-center p-6">
         <div className="grid justify-items-center gap-3">
           <Spinner />
