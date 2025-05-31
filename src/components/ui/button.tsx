2684584bd8745c2237188f7b1956edd93c449ff4
AidanZealley <aidan@zealley.com>
2025-05-31 23:20:30 +0100

    Add mock landing page (#36)
    * Add mock landing page
    
    * Fix type and lint errors

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:bg--events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
@@ -22,6 +22,9 @@ const buttonVariants = cva(
         "ghost-destructive":
           "hover:bg-destructive hover:text-destructive-foreground dark:hover:bg-destructive/50",
         link: "text-primary underline-offset-4 hover:underline",
+        brand: "bg-brand text-brand-foreground hover:bg-brand/90",
+        "brand-inverted":
+          "bg-brand-foreground text-brand hover:bg-brand-foreground/90",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
